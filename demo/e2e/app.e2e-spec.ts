216799aaa09ebf09b1b68bd493833096ba24d6ae
Marco de Freitas <marco.freitas@evodeck.com>
2017-05-16 10:43:15 +0100

    TravisCI Add e2e LinkedIN demo test
    - .travis.yml:
     - Add replace actions for username and password in before_script section to be able to test the demo
    - demo/e2e/app.e2e-spec.ts
     - Change tests to run in sequence due to dependency on LinkedIN library
    - demo/src/app/app.component.html
     - Add ids to login and logout button for e2e demo tests

--- demo/e2e/app.e2e-spec.ts
+++ demo/e2e/app.e2e-spec.ts
@@ -1,25 +1,61 @@
-import { browser, element, by } from 'protractor';
-import { } from 'jasmine';
+import {
+} from 'jasmine';
+import {
+    browser,
+    by,
+    element
+} from 'protractor';
 
 describe('When running the demo', () => {
-    describe('And the login button is clicked', () => {
-        it('should open the login pop up', async () => {
-            browser.get('/');
-            let popUpWindow: any;
-            element(by.id('loginBtt')).click();
-            await browser.getAllWindowHandles().then((handles) => {
-                popUpWindow = handles[1];
-            });
-            expect(popUpWindow).toBeDefined();
-        });
+    const loginButtonId = 'loginBtn';
+
+    it('the login button should be present', () => {
+        const root = '/';
+        browser.get(root);
+        const elem = element(by.id(loginButtonId));
+        expect(elem.isPresent()).toBeTruthy();
     });
 
-    describe('The login button', () => {
-        it('should show up on screen', () => {
-            browser.get('/');
-            const elem = element(by.id('loginBtt'));
-            expect(elem.isPresent()).toBeTruthy();
+    describe('And the login button is clicked', () => {
+        const session_keyName = 'session_key';
+        const session_passwordName = 'session_password';
+        const authorizeName = 'authorize';
+        const timeout = 5000;
+
+        it('should open the login pop up', async () => {
+            element(by.id(loginButtonId)).click();
+            await browser.getAllWindowHandles().then((handles) => {
+                browser.switchTo().window(handles[1]);
+                browser.ignoreSynchronization = true;
+                browser.wait(
+                    () => {
+                        return browser.isElementPresent(by.name(session_keyName))
+                            && browser.isElementPresent(by.name(session_passwordName))
+                            && browser.isElementPresent(by.name(authorizeName));
+                    }
+                    , timeout
+                    , 'LinkedIN popup not loaded or not all elements found.');
+            });
         });
 
+        describe('To perform a login', () => {
+            it('should login without timeout', async () => {
+                const logoutButtonId = 'logoutBtn';
+                const username = 'USERNAME';
+                const password = 'PASSWORD';
+                browser.findElement(by.name(session_keyName)).sendKeys(username);
+                browser.findElement(by.name(session_passwordName)).sendKeys(password);
+                browser.findElement(by.name(authorizeName)).click();
+                await browser.getAllWindowHandles().then((handles) => {
+                    browser.switchTo().window(handles[0]);
+                });
+                browser.wait(
+                    () => {
+                        return browser.findElement(by.id(logoutButtonId)).isDisplayed();
+                    }
+                    , timeout
+                    , 'Login failed or not loaded in time.');
+            });
+        });
     });
 });

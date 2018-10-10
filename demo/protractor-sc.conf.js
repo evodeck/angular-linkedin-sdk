837b44776e84054c9cdf4d673f0051926a75c785
Gustavo Jordão <gustavo.jordao@evodeck.com>
2018-10-10 12:28:18 +0100

    Revert protractor version to see if it runs in Travis

--- demo/protractor-sc.conf.js
+++ demo/protractor-sc.conf.js
@@ -1,38 +1,42 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-const { SpecReporter } = require('jasmine-spec-reporter');
+const { SpecReporter } = require("jasmine-spec-reporter");
 
 var configuration = {
   allScriptsTimeout: 11000,
-  sauceUser: 'User',
-  sauceKey: 'Key',
-  specs: [
-    './tsc/*.e2e-spec.js'
+  sauceUser: "User",
+  sauceKey: "Key",
+  specs: ["./tsc/*.e2e-spec.js", "./tsc/**/*.e2e-spec.js"],
+  baseUrl: "http://localhost:4000",
+  multiCapabilities: [
+    {
+      "tunnel-identifier": "Test",
+      name: "angular-linkedin-sdk demo Firefox E2E chrome",
+      browserName: "chrome"
+    },
+    {
+      "tunnel-identifier": "Test",
+      name: "angular-linkedin-sdk demo E2E firefox",
+      browserName: "firefox"
+    },
+    {
+      "tunnel-identifier": "Test",
+      name: "angular-linkedin-sdk demo E2E IE11",
+      browserName: "internet explorer"
+    }
   ],
-  baseUrl: 'http://localhost:4000',
-  multiCapabilities: [{
-    'tunnel-identifier': 'Test',
-    'name': 'angular-linkedin-sdk demo Firefox E2E chrome',
-    'browserName': 'chrome'
-  }, {
-    'tunnel-identifier': 'Test',
-    'name': 'angular-linkedin-sdk demo E2E firefox',
-    'browserName': 'firefox'
-  }, {
-    'tunnel-identifier': 'Test',
-    'name': 'angular-linkedin-sdk demo E2E IE11',
-    'browserName': 'internet explorer'
-  }],
   directConnect: false,
-  framework: 'jasmine',
+  framework: "jasmine",
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
-    print: function () { }
+    print: function() {}
   },
   onPrepare() {
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+    jasmine
+      .getEnv()
+      .addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
   }
 };
 exports.config = configuration;
